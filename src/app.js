3f0e585529312f00139cebcc8f14dc5d8586a5c5
aditya <4wastestuff@gmail.com>
2019-12-23 15:01:49 +0530

    update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const hbs = require('hbs')
 
 console.log(__dirname)
 const pathfile = path.join(__dirname,'../public')
-const viewsPath ='/home/adithya/firstapp/web-server/templates/views'
+const viewsPath = path.join(__dirname,'../templates/views')
 const partialpath = '/home/adithya/firstapp/web-server/templates/partials'
 console.log(partialpath)
 
